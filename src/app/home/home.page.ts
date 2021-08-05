f603976e4bd0600de7fd95c707fa1015b86e1e6a
HwaA18 <hwaa18@gmail.com>
2021-08-05 13:11:45 -0400

    First commit

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { AlertController } from '@ionic/angular';
+import { HttpClient } from '@angular/common/http';
+import { setClassMetadata } from '@angular/core/src/r3_symbols';
 
 @Component({
   selector: 'app-home',
@@ -7,6 +10,72 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
-  constructor() {}
+  searched: String;
+  searchString: String;
+  lat: number;
+  long: number;
+  showResults: boolean;
+  covidData: any = false;
+  totalCases: number;
+  newCases: number;
+  deaths: number;
+  newDeaths: number;
+  recovered: number;
+  active: number;
+  critical: number;
+  casesPer1Mill: number;
 
+  constructor(private http: HttpClient, private alertController: AlertController) { }
+  
+  async getInfo() {
+
+    this.searchString = this.searched;
+    let httpString = "https://coronavirus-19-api.herokuapp.com/countries/" + this.searchString;
+
+    /*await this.http.get(httpString).subscribe((response) => {
+      this.covidData = response;
+      //console.log(response);
+    }); */
+
+    try {
+      const response = await fetch(httpString);
+      const json = await response.json();
+      //console.log(json);
+
+      this.covidData = json;
+      this.showResults = true;
+    } catch (error) {
+      console.log(error)
+      this.unrecognizedLocation();
+    } finally {}
+
+    console.log(this.covidData)
+    
+    this.totalCases = this.covidData['cases'];
+    this.newCases = this.covidData['todayCases'];
+    this.deaths = this.covidData['deaths'];
+    this.newDeaths = this.covidData['todayDeaths'];;
+    this.recovered = this.covidData['recovered'];;
+    this.active = this.covidData['active'];;
+    this.critical = this.covidData['critical'];;
+    this.casesPer1Mill = this.covidData['casesPerOneMillion'];;
+  }
+
+  async unrecognizedLocation() {
+        const alert = await this.alertController.create({
+          header: 'Alert',
+          subHeader: 'Unrecognized Location',
+          message: 'We were un able to find the specified country. Please try again.',
+          buttons: [
+            {
+              text: 'Ok',
+              handler: () => {
+                //this.startOver();
+              }
+            }
+          ]
+        });
+    
+        await alert.present();
+      }
 }
